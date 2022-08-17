65c884d9efb5dc6b7ffcc14324fc48ce966a3373
= <minhngo2814@gmail.com>
2022-08-16 22:37:55 -0700

    icon link button and bottom footer

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -1,11 +1,18 @@
 import styles from "../styles/Home.module.css";
 import { Container, Box, Grid, Link } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import { Icon } from "@mui/material";
 
 // Styling
+const footerStyle = {
+  position: "fixed",
+  bottom: 0,
+  width: "100%",
+};
+
 const linkStyle = {
   padding: "20px",
   cursor: "pointer",
@@ -20,12 +27,12 @@ const boxStyle = {
 };
 
 const iconStyle = {
-  margin: "10px",
+  color: "white",
 };
 
 const Footer = () => {
   return (
-    <Box bgcolor="primary.main">
+    <Box bgcolor="primary.main" sx={footerStyle}>
       <Container maxWidth="lg">
         <Box component="span" sx={boxStyle}>
           <Link sx={linkStyle}>WebDesign</Link>
@@ -36,9 +43,15 @@ const Footer = () => {
           <Link sx={linkStyle}>Languages</Link>
         </Box>
         <Box component="span" sx={boxStyle}>
-          <GitHubIcon sx={iconStyle} />
-          <LinkedInIcon sx={iconStyle} />
-          <TwitterIcon sx={iconStyle} />
+          <IconButton href="#" target="_blank">
+            <GitHubIcon sx={iconStyle} />
+          </IconButton>
+          <IconButton href="#" target="_blank">
+            <LinkedInIcon sx={iconStyle} />
+          </IconButton>
+          <IconButton href="#" target="_blank">
+            <TwitterIcon sx={iconStyle} />
+          </IconButton>
         </Box>
         <Box component="span" sx={boxStyle}>
           <p>&lt; &copy; Power By Django and Next.js &gt;</p>
