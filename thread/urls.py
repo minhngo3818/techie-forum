ac07eea0b7723f45d15bec2660bb0bd760a56170
= <minhngo2814@gmail.com>
2022-08-21 21:29:37 -0700

    register urls

--- thread/urls.py
+++ thread/urls.py
@@ -2,8 +2,14 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.permissions import AllowAny
 from django.conf import settings
-# import views here
+from .views import ThreadViews, CommentViews
 
 router = DefaultRouter()
 # Register url here
+router.register(r"thread", ThreadViews)
+router.register(r"comment", CommentViews)
 
+
+urlpatterns = [
+    path("forum/", include(router.urls))
+]
