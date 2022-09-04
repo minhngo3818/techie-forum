5a06a3693c302237dac78259dcb0b6c30ab3386d
= <minhngo2814@gmail.com>
2022-09-04 14:22:37 -0700

    change background

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -4,9 +4,7 @@ import { Box } from "@mui/material";
 
 const Layout = ({ children }) => {
   return (
-    <Box
-      sx={{ backgroundColor: "#21262d", minHeight: "1200px", width: "100%" }}
-    >
+    <Box sx={{ backgroundColor: "#000", minHeight: "1200px", width: "100%" }}>
       <Navigation />
       <main>{children}</main>
       <Footer />
