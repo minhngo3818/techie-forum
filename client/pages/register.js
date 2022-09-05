acb5617dfbd8fae051167c1753a8288f4f44ed8f
= <minhngo2814@gmail.com>
2022-09-04 20:33:09 -0700

    registeration feature & styling

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,25 +1,40 @@
-import Link from "next/link";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useReducer, useContext } from "react";
 import AuthContext from "../services/auth/AuthService";
 import PageHeader from "../components/PageHeader";
 import { Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faCircleXmark,
+} from "@fortawesome/free-regular-svg-icons";
 import styles from "../styles/Register.module.css";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,30}$/;
+
+function selectValidIcon(state, action) {}
 
 const Register = () => {
   const { register } = useContext(AuthContext);
   const [success, setSuccess] = useState(false);
-  const [isPasswordMatch, setPasswordMatch] = useState(false);
   const [signUp, setSignUp] = useState({
     username: "",
     email: "",
     password: "",
     password2: "",
   });
+  // Validation states
+  const [isValidName, setIsValidName] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [isPasswordMatch, setPasswordMatch] = useState(false);
 
   useEffect(() => {
+    setIsValidName(USER_REGEX.test(signUp.username));
+  }, [signUp.username]);
+
+  useEffect(() => {
+    setIsValidPassword(PASSWORD_REGEX.test(signUp.password));
     setPasswordMatch(signUp.password === signUp.password2);
   }, [signUp.password, signUp.password2]);
 
@@ -33,14 +48,14 @@ const Register = () => {
         password2: signUp.password2,
       });
     }
-
+    setSuccess(true);
     // Refresh signUp state
     setSignUp({
       username: "",
       email: "",
       password: "",
-      password2: ""
-    })
+      password2: "",
+    });
   };
 
   return (
@@ -52,44 +67,134 @@ const Register = () => {
         onSubmit={handleSignUp}
       >
         <Form.Group className={styles.group}>
-          <Form.Label className={styles.label}>Username</Form.Label>
+          <Form.Label className={styles.label}>
+            Username
+            <FontAwesomeIcon
+              icon={faCircleCheck}
+              className={isValidName ? styles.valid : styles.hide}
+            />
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className={
+                isValidName || !signUp.username ? styles.hide : styles.invalid
+              }
+            />
+          </Form.Label>
           <Form.Control
             className={styles.formControl}
             name="username"
             type="text"
-            onChange={(e) => setSignUp({ username: e.target.value })}
+            onChange={(e) => setSignUp({ ...signUp, username: e.target.value })}
+            required
           ></Form.Control>
+          <p
+            className={
+              isValidName || !signUp.username ? styles.hide : styles.invalidNote
+            }
+          >
+            Username must contain 4 to 24 characters
+            <br />
+            Must start with a letter
+            <br />
+            Special characters are not allowed except hypen and underscore
+          </p>
         </Form.Group>
+
         <Form.Group className={styles.group}>
           <Form.Label className={styles.label}>Email</Form.Label>
           <Form.Control
             className={styles.formControl}
             name="email"
             type="text"
-            onChange={(e) => setSignUp({ email: e.target.value })}
+            onChange={(e) => setSignUp({ ...signUp, email: e.target.value })}
+            required
           ></Form.Control>
         </Form.Group>
+
         <Form.Group className={styles.group}>
-          <Form.Label className={styles.label}>Password</Form.Label>
+          <Form.Label className={styles.label}>
+            Password
+            <FontAwesomeIcon
+              icon={faCircleCheck}
+              className={isValidPassword ? styles.valid : styles.hide}
+            />
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className={
+                isValidPassword || !signUp.password
+                  ? styles.hide
+                  : styles.invalid
+              }
+            />
+          </Form.Label>
           <Form.Control
             className={styles.formControl}
             name="password"
             type="password"
-            onChange={(e) => setSignUp({ password: e.target.value })}
+            onChange={(e) => setSignUp({ ...signUp, password: e.target.value })}
+            required
           ></Form.Control>
+          <p
+            className={
+              isValidPassword || !signUp.password
+                ? styles.hide
+                : styles.invalidNote
+            }
+          >
+            Password must contain at least 8 characters
+            <br />
+            Include upper & lowercase letters, a number & a special character.
+            <br />
+            Allowed special characters:
+            <span aria-label="exclamation mark">!</span>
+            <span aria-label="at symbol">@</span>
+            <span aria-label="hashtag">#</span>
+            <span aria-label="dollar sign">$</span>
+            <span aria-label="percent">%</span>
+          </p>
         </Form.Group>
+
         <Form.Group className={styles.group}>
-          <Form.Label className={styles.label}>Confirm Password</Form.Label>
+          <Form.Label className={styles.label}>
+            Confirm Password
+            <FontAwesomeIcon
+              icon={faCircleCheck}
+              className={
+                isPasswordMatch && signUp.password2 ? styles.valid : styles.hide
+              }
+            />
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className={
+                isPasswordMatch || !signUp.password2
+                  ? styles.hide
+                  : styles.invalid
+              }
+            />
+          </Form.Label>
           <Form.Control
             className={styles.formControl}
             name="confirmPassword"
             type="password"
-            onChange={(e) => setSignUp({ password2: e.target.value })}
+            onChange={(e) =>
+              setSignUp({ ...signUp, password2: e.target.value })
+            }
+            required
           ></Form.Control>
+          <p
+            className={
+              isPasswordMatch || !signUp.password2
+                ? styles.hide
+                : styles.invalidNote
+            }
+          >
+            Two password fields don't match!
+          </p>
         </Form.Group>
+
         <Form.Group className={styles.btnContainer}>
           <Button className={styles.btn} type="submit">
-            Register
+            Sign-Up
           </Button>
         </Form.Group>
       </Form>
