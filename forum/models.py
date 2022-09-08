19fd3213f4638781ecf34d2e6eaf7fb3a43caf5f
= <minhngo2814@gmail.com>
2022-09-07 21:01:24 -0700

    modify models

--- forum/models.py
+++ forum/models.py
@@ -82,6 +82,7 @@ class LikeComment(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self) -> str:
--- user/models.py
+++ user/models.py
@@ -15,10 +15,24 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    firstName = models.CharField(max_length=100, null=True, blank=True)
-    lastName = models.CharField(max_length=100, null=True, blank=True)
+
+    # self
+    display_name = models.CharField(max_length=100, null=True, blank=True)
+    display_name_id = models.IntegerField(default=1000, null=True, blank=True, unique=True)  # use in put request
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_last = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     avatar = models.ImageField(upload_to='images', null=True, blank=True)   # add default avatar
+
+    # social media
+    twitter_url = models.URLField(null=True, blank=True)
+    reddit_url = models.URLField(null=True, blank=True)
+    github_url = models.URLField(null=True, blank=True)
+    stackoverflow_url = models.URLField(null=True, blank=True)
+    linkedin_url = models.URLField(null=True, blank=True)
+    indeed_url = models.URLField(null=True, blank=True)
+
+    # others
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
