e9bf56b7218031ffad8d215c8403cb5ccd2cd8d9
minhngo <minhngo2814@gmail.com>
2022-09-09 09:47:11 -0700

    register Tag in admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Thread, Comment, Like, LikeComment
+from .models import Thread, Comment, Like, LikeComment, Tag
 # Register your models here.
 admin.site.register(Thread)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(LikeComment)
+admin.site.register(Tag)
