662f28c906424327df3dda1d8582b1f259d4846e
= <minhngo2814@gmail.com>
2022-09-10 12:03:03 -0700

    increase token time for dev

--- techies/settings.py
+++ techies/settings.py
@@ -158,8 +158,8 @@ REST_USE_JWT = True
 REST_AUTH_TOKEN_MODEL = None
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  # increase time before deploying
-    "REFRESH_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  # increase time before deploying
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
