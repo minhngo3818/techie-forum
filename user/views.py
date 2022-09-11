c001ec57221611769dd0bb31d6e4f22c730e3dbc
= <minhngo2814@gmail.com>
2022-09-10 23:58:01 -0700

    add admin permission in profile view

--- user/views.py
+++ user/views.py
@@ -64,7 +64,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
