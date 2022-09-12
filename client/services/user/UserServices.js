e824b27f72ce2f42bb6a2a82e9b469cb57bb86ba
= <minhngo2814@gmail.com>
2022-09-11 21:26:54 -0700

    get user data

--- client/services/user/UserServices.js
+++ client/services/user/UserServices.js
@@ -1,18 +1,39 @@
 import { useRouter } from "next/router";
 import axiosInstance from "../axios";
+import { createContext } from "react";
+import jwtDecode from "jwt-decode";
 
-const getUser = async (url, accessToken) => {
-  const response = await axiosInstance.get(url, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+const UserContext = createContext();
+
+const getUser = async (accessToken) => {
+  let response = await axiosInstance.get(
+    `user-view/${jwtDecode(accessToken)}/`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  return response.data;
+};
+
+const getProfile = async (accessToken) => {
+  let response = await axiosInstance.get(
+    `profile-view/${jwtDecode(accessToken)}/`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
   return response.data;
 };
 
 const UserService = {
   getUser,
+  getProfile,
 };
 
 export default UserService;
