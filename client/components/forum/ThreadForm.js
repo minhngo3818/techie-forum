abd81950f624732bc0df9783caaeb3d5084772d8
= <minhngo2814@gmail.com>
2022-09-15 01:35:58 -0700

    add tweet form

--- client/components/forum/ThreadForm.js
+++ client/components/forum/ThreadForm.js
@@ -1,35 +1,44 @@
 import { useState, useRef, useEffect } from "react";
-import { ToggleButton } from "react-bootstrap";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ImageButton from "./buttons/ImageButton";
+import { Form, Button } from "react-bootstrap";
 import styles from "../../styles/ThreadForm.module.css";
 import customBS from "../../styles/CustomBootstrap.module.css";
+import EmojiButton from "./buttons/EmojiButton";
 
 const ThreadForm = (props) => {
   return (
-    <>
-      <div
-        className={props.isOpen ? styles.postFormOpen : styles.postFormClose}
-      >
-        <CKEditor
-          editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
-          onReady={(editor) => {
-            console.log("Editor is ready to use!", editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log("Focus.", editor);
-          }}
-        />
-      </div>
-    </>
+    <div className={props.isOpen ? styles.postFormOpen : styles.postFormClose}>
+      <Form className={styles.Form}>
+        <Form.Label className={styles.FormButtons}>
+          <ImageButton />
+          <EmojiButton />
+        </Form.Label>
+        <Form.Group>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            className={styles.FormControl}
+            placeholder="What's poppin, eh?"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Your Tweet</Form.Label>
+          <Form.Control
+            className={styles.FormControl}
+            placeholder="What's poppin, eh?"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Tags</Form.Label>
+          <Form.Control
+            className={styles.FormControl}
+            placeholder="What's poppin, eh?"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Button>Post</Button>
+        </Form.Group>
+      </Form>
+    </div>
   );
 };
 
