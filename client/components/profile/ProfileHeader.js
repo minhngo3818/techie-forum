6b471d8697956e36e7712b01b3d2ab32780898a3
= <minhngo2814@gmail.com>
2022-09-14 18:29:49 -0700

    edit avatar ui

--- client/components/profile/ProfileHeader.js
+++ client/components/profile/ProfileHeader.js
@@ -1,6 +1,7 @@
-import { Modal, Button } from "react-bootstrap";
-import { useState, useRef, useContext } from "react";
+import { useState, useRef, useContext, useEffect } from "react";
 import AuthContext from "../../services/auth/AuthService";
+import { Modal, Button, Col, Row } from "react-bootstrap";
+import RangeSlider from "react-bootstrap-range-slider";
 import AvatarEditor from "react-avatar-editor";
 import customBS from "../../styles/CustomBootstrap.module.css";
 import styles from "../../styles/Profile.module.css";
@@ -10,32 +11,45 @@ const ProfileHeader = () => {
   const [editAvatar, setEditAvatar] = useState(false);
 
   // Edit avatar handlers
-  const editor = useRef(null);
+  const [scale, setScale] = useState(parseFloat(1.0).toFixed(2));
+  const [rotate, setRotate] = useState(parseInt(0));
   const showEditAvatar = () => setEditAvatar(true);
   const closeEditAvatar = () => setEditAvatar(false);
+  const handleValue = (e) => {
+    scaleRef.current = e.target.value;
+  };
 
   return (
     <div className={styles.header}>
       <div className={styles.headerUrls}>
-        {profile !== null && profile.twitter_url !== null && (
-          <a href={profile.twitter_url}>Twitter</a>
-        )}
-        {profile !== null && profile.reddit_url !== null && (
-          <a href={profile.reddit_url}>Reddit</a>
-        )}
-        {profile !== null && profile.stackoverflow_url !== null && (
-          <a href={profile.stackoverflow_url}>Stack Overflow</a>
-        )}
-        {profile !== null && profile.linkedin_url !== null && (
-          <a href={profile.linkedin_url}>Linkedin</a>
-        )}
-        {profile !== null && profile.indeed_url !== null && (
-          <a href={profile.indeed_url}>Indeed</a>
-        )}
-        {profile?.github_url !== null && (
-          <a href={profile?.github_url}>Github</a>
-        )}
+        <div className={styles.urlsGrid}>
+          {profile !== null && profile.twitter_url !== null && (
+            <a href={profile.twitter_url}>Twitter</a>
+          )}
+          {profile !== null && profile.reddit_url !== null && (
+            <a id={styles.right} href={profile.reddit_url}>
+              Reddit
+            </a>
+          )}
+          {profile !== null && profile.stackoverflow_url !== null && (
+            <a href={profile.stackoverflow_url}>Stack Overflow</a>
+          )}
+          {profile !== null && profile.linkedin_url !== null && (
+            <a id={styles.right} href={profile.linkedin_url}>
+              Linkedin
+            </a>
+          )}
+          {profile !== null && profile.indeed_url !== null && (
+            <a href={profile.indeed_url}>Indeed</a>
+          )}
+          {profile?.github_url !== null && (
+            <a id={styles.right} href={profile?.github_url}>
+              Github
+            </a>
+          )}
+        </div>
       </div>
+      2
       <div className={styles.headerAvatar}>
         <img
           src={profile?.avatar}
@@ -50,20 +64,52 @@ const ProfileHeader = () => {
         >
           <Modal.Body className={customBS.modalBody}>
             <AvatarEditor
-              ref={editor}
               image={profile?.avatar}
               width={250}
               height={250}
               border={50}
               borderRadius={150}
-              scale={1.2}
+              scale={scale}
+              rotate={rotate}
             />
+            <Row className={styles.avatarEditRow}>
+              <Col className={styles.avatarEdit}>
+                <p>Zoom: {scale}</p>
+              </Col>
+              <Col xs={8}>
+                <RangeSlider
+                  variant="dark"
+                  value={scale}
+                  max={2.5}
+                  min={1.0}
+                  step={0.05}
+                  onChange={(e) => setScale(e.target.value)}
+                  tooltip="off"
+                />
+              </Col>
+            </Row>
+            <Row className={styles.avatarEditRow}>
+              <Col className={styles.avatarEdit}>
+                <p>Rotate: {rotate}&deg;</p>
+              </Col>
+              <Col xs={8}>
+                <RangeSlider
+                  variant="dark"
+                  value={rotate}
+                  max={360}
+                  min={0}
+                  step={1}
+                  onChange={(e) => setRotate(e.target.value)}
+                  tooltip="off"
+                />
+              </Col>
+            </Row>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={closeEditAvatar}>
               Close
             </Button>
-            <Button variant="primary">Save Changes</Button>
+            <Button variant="dark">Save Changes</Button>
           </Modal.Footer>
         </Modal>
 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
 import SSRProvider from "react-bootstrap/SSRProvider";
 import { AuthProvider } from "../services/auth/AuthService";
 import { ToastContainer } from "react-toastify";
