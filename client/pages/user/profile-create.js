18e5d5a625ca67268f2fb884132b40c095787658
= <minhngo2814@gmail.com>
2022-09-20 21:30:59 -0700

    profile-create ui

--- client/pages/user/profile-create.js
+++ client/pages/user/profile-create.js
@@ -1,18 +1,103 @@
+import { style } from "@mui/system";
+import { useState, useRef } from "react";
 import { Form, Button, Carousel } from "react-bootstrap";
 import PageHeader from "../../components/PageHeader";
 import AuthGuard from "../../services/auth/AuthGuard";
+import customBS from "../../styles/CustomBootstrap.module.css";
+import styles from "../../styles/ProfileForm.module.css";
 
-// This page for new user to create their profile
+const refLinks = [
+  { name: "Twitter", url: "https://twitter.com/your-username" },
+  { name: "Reddit", url: "https://reddit.com/your-username" },
+  { name: "Stackoverflow", url: "https://stackoverflow.com/your-username" },
+  { name: "Linkedin", url: "https://linkedin.com/in/your-username" },
+  { name: "Indeed", url: "https://indeed.com/your-username" },
+  { name: "Github", url: "https://github.com/your-username" },
+];
 
-// If each page are filled in its fields
-// Then allow to move to next crousel
-// Add loading state with different message between two page
-// Estimate 3 pages forms
 const ProfileCreate = () => {
+  const displayNameRef = useRef();
+  const bioRef = useRef();
+
+  const [displayName, setDisplayName] = useState(null);
+  const [bio, setBio] = useState(null);
+
+  const [allowNextPage, setAllowNextPage] = useState(false);
+
+  const handleNextPage = () => {
+    if (displayName !== null && bio !== null) {
+      setAllowNextPage(true);
+    }
+  };
+
   return (
-    <>
-      <PageHeader pageName="Profile Create Page" />
-    </>
+    <div className={styles.container}>
+      <PageHeader pageName="Create Your Profile" />
+      <Form>
+        <Carousel
+          className={styles.carouselContainer}
+          interval={null}
+          controls={false}
+          wrap={false}
+        >
+          <Carousel.Item className={styles.itemContainer}>
+            <Form.Group className={styles.formGroup}>
+              <Form.Label className={styles.formLabel}>Avatar</Form.Label>
+              <Form.Control
+                className={customBS.formControl}
+                type="file"
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formGroup}>
+              <Form.Label className={styles.formLabel}>Display Name</Form.Label>
+              <Form.Control
+                className={customBS.formControl}
+                placeholder="display name"
+                requrired
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.formGroup}>
+              <Form.Label className={styles.formLabel}>Bio</Form.Label>
+              <Form.Control
+                as="textarea"
+                className={customBS.formControl}
+                placeholder="Add your biography"
+                rows={7}
+              ></Form.Control>
+            </Form.Group>
+          </Carousel.Item>
+          <Carousel.Item className={styles.itemContainer}>
+            <div className={styles.linksContainer}></div>
+            {refLinks.map((refLink) => {
+              return (
+                <Form.Group className={styles.formGroup}>
+                  <Form.Label className={styles.formLabel}>
+                    {refLink.name}
+                  </Form.Label>
+                  <Form.Control
+                    className={customBS.formControl}
+                    placeholder={refLink.url}
+                  ></Form.Control>
+                </Form.Group>
+              );
+            })}
+          </Carousel.Item>
+          <Carousel.Item className={styles.itemContainer}>
+            <Form.Group className={styles.submitSection}>
+              <Form.Label className={styles.formLabel}>
+                Have you finished ?
+              </Form.Label>
+              <Form.Label className={styles.formLabel}>
+                Go back and change your info if you're not done yet
+              </Form.Label>
+              <Button type="submit" className={styles.submitButton}>
+                Submit
+              </Button>
+            </Form.Group>
+          </Carousel.Item>
+        </Carousel>
+      </Form>
+    </div>
   );
 };
 
