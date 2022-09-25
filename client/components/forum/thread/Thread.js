e541757d4f86b02ed4401be482a82b660222ef34
= <minhngo2814@gmail.com>
2022-09-25 16:24:59 -0700

    refactor Threads

--- client/components/forum/thread/Thread.js
+++ client/components/forum/thread/Thread.js
@@ -1,20 +1,51 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faHeart,
-  faMessage,
-  faPenToSquare,
-} from "@fortawesome/free-regular-svg-icons";
-import { faShareNodes } from "@fortawesome/free-solid-svg-icons";
-import styles from "../../../styles/Thread.module.css";
+  LikeButton,
+  EditButton,
+  ShareButton,
+  CommentButton,
+} from "./button/Buttons";
+import { Form } from "react-bootstrap";
+import customBS from "../../../styles/CustomBootstrap.module.css";
+import styles from "./Thread.module.css";
 
 // Dummy tags
 const tags = ["C++", "Console App", "System", "Algorithm", "Server"];
 
-// Props
 const Thread = (props) => {
+  // States
+  const [isLiked, setIsLiked] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isComment, setIsComment] = useState(false);
+  const [editThread, setEditThread] = useState({
+    title: props.title,
+    content: props.content,
+  });
+  const editRef = useRef();
+
+  // Handlers
+  const handleLike = () => {
+    setIsLiked((prev) => !prev);
+  };
+
+  const handleIsEdit = () => {
+    if (isEdit) {
+      setEditThread({ title: props.title, content: props.content });
+    }
+    setIsEdit((prev) => !prev);
+  };
+
+  const handleComment = () => {
+    setIsComment((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isEdit) {
+      editRef.current.focus();
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -23,8 +54,42 @@ const Thread = (props) => {
         <p>Posted on {props.created}</p>
       </div>
       <div className={styles.content}>
-        <h3>Making a super quantum computer</h3>
-        <p>{props.content}</p>
+        {isEdit ? (
+          <Form>
+            <Form.Group className={styles.editGroup}>
+              <Form.Control
+                ref={editRef}
+                className={customBS.formControl}
+                id={customBS.title}
+                as="input"
+                value={editThread.title}
+                onChange={(e) =>
+                  setEditThread({ ...editThread, title: e.target.value })
+                }
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className={styles.editGroup}>
+              <Form.Control
+                ref={editRef}
+                className={customBS.formTextarea}
+                as="textarea"
+                value={editThread.content}
+                onChange={(e) =>
+                  setEditThread({ ...editThread, content: e.target.value })
+                }
+                rows="4"
+                cols="50"
+              >
+                {editThread.content}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        ) : (
+          <div aria-label="render thread data">
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+          </div>
+        )}
       </div>
       <div className={styles.tags}>
         {tags.map((tag) => {
@@ -32,42 +97,10 @@ const Thread = (props) => {
         })}
       </div>
       <div className={styles.buttonList}>
-        <OverlayTrigger
-          className={styles.button}
-          placement="top"
-          overlay={<Tooltip>Like</Tooltip>}
-        >
-          <Button type="button" className={styles.button}>
-            <FontAwesomeIcon className={styles.icon} icon={faHeart} />
-          </Button>
-        </OverlayTrigger>
-        <OverlayTrigger
-          className={styles.button}
-          placement="top"
-          overlay={<Tooltip>Share</Tooltip>}
-        >
-          <Button type="button" className={styles.button}>
-            <FontAwesomeIcon className={styles.icon} icon={faShareNodes} />
-          </Button>
-        </OverlayTrigger>
-        <OverlayTrigger
-          className={styles.button}
-          placement="top"
-          overlay={<Tooltip>Edit</Tooltip>}
-        >
-          <Button type="button" className={styles.button}>
-            <FontAwesomeIcon className={styles.icon} icon={faPenToSquare} />
-          </Button>
-        </OverlayTrigger>
-        <OverlayTrigger
-          className={styles.button}
-          placement="top"
-          overlay={<Tooltip>Comment</Tooltip>}
-        >
-          <Button type="button" className={styles.button}>
-            <FontAwesomeIcon className={styles.icon} icon={faMessage} />
-          </Button>
-        </OverlayTrigger>
+        <LikeButton isLiked={isLiked} onChange={handleLike} />
+        <ShareButton />
+        <EditButton isEdit={isEdit} onChange={handleIsEdit} />
+        <CommentButton isComment={isComment} onChange={handleComment} />
       </div>
     </div>
   );
