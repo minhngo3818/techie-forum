61385c666c5b593ab42010e1bb3ac81c0e35227b
= <minhngo2814@gmail.com>
2022-09-25 12:36:35 -0700

    refactor layout

--- client/components/layout/Layout.js
+++ client/components/layout/Layout.js
@@ -3,7 +3,9 @@ import Footer from "../footer/Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div style={{ backgroundColor: "#000", minHeight: "1200px", width: "100%" }}>
+    <div
+      style={{ backgroundColor: "#000", minHeight: "1200px", width: "100%" }}
+    >
       <Navigation />
       <main>{children}</main>
     </div>
