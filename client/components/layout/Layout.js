8b6be43f3c82d845a8a24d73f793a0ec6db5cc4a
= <minhngo2814@gmail.com>
2022-09-25 12:24:41 -0700

    remove mui from layout

--- client/components/layout/Layout.js
+++ client/components/layout/Layout.js
@@ -1,13 +1,12 @@
 import Navigation from "../navbar/Navbar";
 import Footer from "../footer/Footer";
-import { Box } from "@mui/material";
 
 const Layout = ({ children }) => {
   return (
-    <Box sx={{ backgroundColor: "#000", minHeight: "1200px", width: "100%" }}>
+    <div style={{ backgroundColor: "#000", minHeight: "1200px", width: "100%" }}>
       <Navigation />
       <main>{children}</main>
-    </Box>
+    </div>
   );
 };
 
