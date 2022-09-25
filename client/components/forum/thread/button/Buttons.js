97034b2b33d5c99b4becf51db78cea0d17b1bf52
= <minhngo2814@gmail.com>
2022-09-25 16:25:31 -0700

    thread buttons

---
+++ client/components/forum/thread/button/Buttons.js
@@ -0,0 +1,94 @@
+import PropTypes from "prop-types";
+import { OverlayTrigger, Button, Tooltip } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faPenToSquare,
+  faMessage,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faShareNodes,
+  faHeart as solidHeart,
+  faPenToSquare as solidPenToSquare,
+  faMessage as solidMessage,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "../Thread.module.css";
+
+const LikeButton = (props) => {
+  return (
+    <OverlayTrigger placement="top" overlay={<Tooltip>Like</Tooltip>}>
+      <Button type="button" className={styles.button} onClick={props.onChange}>
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={props.isLiked ? solidHeart : faHeart}
+          flip
+        />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+LikeButton.propTypes = {
+  isLiked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const EditButton = (props) => {
+  return (
+    <OverlayTrigger placement="top" overlay={<Tooltip>Edit</Tooltip>}>
+      <Button
+        type="button"
+        className={styles.button}
+        id={props.isEdit ? styles.active : null}
+        onClick={props.onChange}
+      >
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={props.isEdit ? solidPenToSquare : faPenToSquare}
+          flip
+        />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+EditButton.propTypes = {
+  isEdit: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const ShareButton = (props) => {
+  return (
+    <OverlayTrigger placement="top" overlay={<Tooltip>Share</Tooltip>}>
+      <Button type="button" className={styles.button}>
+        <FontAwesomeIcon className={styles.icon} icon={faShareNodes} flip />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+const CommentButton = (props) => {
+  return (
+    <OverlayTrigger placement="top" overlay={<Tooltip>Comment</Tooltip>}>
+      <Button
+        type="button"
+        className={styles.button}
+        id={props.isComment ? styles.active : null}
+        onClick={props.onChange}
+      >
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={props.isComment ? solidMessage : faMessage}
+          flip
+        />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+CommentButton.propTypes = {
+  isComment: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export { LikeButton, EditButton, ShareButton, CommentButton };
