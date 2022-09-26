a68ae4f4c7b64d9525c5f28517965f7b45f74995
= <minhngo2814@gmail.com>
2022-09-26 11:05:10 -0700

    purge footer

--- client/components/footer/Footer.js
+++ client/components/footer/Footer.js
@@ -1,53 +1,7 @@
-import { Container, Box } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import TwitterIcon from "@mui/icons-material/Twitter";
-
 // TODO: Replace MUI ui with bootstrap
-const footerStyle = {
-  position: "relative",
-  bottom: 0,
-  width: "100%",
-  flexGrow: 0,
-  display: { xs: "flex", md: "flex" },
-};
-
-const boxStyle = {
-  display: "flex",
-  justifyContent: "center",
-  color: "black",
-  margin: "10px auto",
-};
-
-const iconStyle = {
-  color: "black",
-};
 
 const Footer = () => {
-  return (
-    <Box bgcolor="white" sx={footerStyle}>
-      <Container maxWidth="lg">
-        <Box sx={boxStyle}>
-          <Box>Devloped by Minh Tuyen Ngo</Box>
-        </Box>
-        <Box component="span" sx={boxStyle}>
-          <IconButton href="#" target="_blank">
-            <GitHubIcon sx={iconStyle} />
-          </IconButton>
-          <IconButton href="#" target="_blank">
-            <LinkedInIcon sx={iconStyle} />
-          </IconButton>
-          <IconButton href="#" target="_blank">
-            <TwitterIcon sx={iconStyle} />
-          </IconButton>
-        </Box>
-        <Box component="span" sx={boxStyle}>
-          <p> &copy; Power By Django and Next.js </p>
-        </Box>
-      </Container>
-    </Box>
-  );
+  return <>Footer</>;
 };
 
 export default Footer;
