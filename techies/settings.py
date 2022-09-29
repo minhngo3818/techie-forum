8ffb1662981db53d2bdcc252cae5676114421d3c
= <minhngo2814@gmail.com>
2022-09-28 22:16:55 -0700

    replace limitoffset with pagenumber pagination

--- techies/settings.py
+++ techies/settings.py
@@ -205,6 +205,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
