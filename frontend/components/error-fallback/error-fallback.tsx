10d7501341d2ce600f9bdee58780d2351c690d10
minhngo3818 <minhngo2814@gmail.com>
2023-01-18 09:34:08 -0800

    error-boundary

---
+++ frontend/components/error-fallback/error-fallback.tsx
@@ -0,0 +1,14 @@
+import { Warning } from "../icons/icons";
+
+export default function ErrorFallback(props: { error: string }) {
+  return (
+    <div role="alert">
+      <p>Oops..! Something went wrong:</p>
+      <pre>{props.error}</pre>
+      <button>
+        <Warning />
+        Try Again
+      </button>
+    </div>
+  );
+}
