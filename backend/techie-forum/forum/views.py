99d7f537168709d32659a211ce1baab14196ce3f
minhngo3818 <minhngo2814@gmail.com>
2023-01-28 23:54:56 -0800

    like and memorize, settings env

--- backend/techie-forum/forum/views.py
+++ backend/techie-forum/forum/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
+from rest_framework import status
 from user.models import Profile
 from .models import BasePost, Thread, Comment, ParentChildComment, Like, Memorize, Tag
 from .serializers import (
@@ -23,9 +24,9 @@ class ThreadViewSet(viewsets.ModelViewSet):
     pagination_class = PaginationHelper
 
     def get_queryset(self):
-        category = self.request.query_params.get("category", None)
+        category = self.request.query_params.get("category", "")
 
-        if category:
+        if category != "":
             return self.queryset.filter(category=category)
 
         return self.queryset
@@ -45,10 +46,6 @@ class ThreadViewSet(viewsets.ModelViewSet):
         for name in tag_names:
             Tag.objects.get_or_create(name=name)
 
-        # Quest to before implement add memorize
-        # Where should I place the update: Memorized view or Thread view
-        # How can I perform the delete
-
         serializer.save(owner=self.request.user.profile, updated_at=timezone.now())
 
 
@@ -59,7 +56,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     pagination_class = PaginationHelper
 
     def get_queryset(self):
-        thread_id = self.request.GET.get("thid", "")
+        thread_id = self.request.GET.get("tid", "")
         parent_id = self.request.GET.get("pcid", "")
         depth = self.request.GET.get("depth", 0)
         comment_pairs = ParentChildComment.objects.filter(parent=parent_id)
@@ -71,7 +68,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         ).defer("content")
 
     def perform_create(self, serializer):
-        thread_id = self.request.POST.get("thid", "")
+        thread_id = self.request.POST.get("tid", "")
         parent_id = self.request.POST.get("pcid", "")
         depth = self.request.POST.get("depth", 0)
         serializer.save(owner=self.request.user.profile, _thread=thread_id, depth=depth)
@@ -89,35 +86,67 @@ class LikeViewSet(viewsets.ModelViewSet):
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_fields = ("pid", "post_id")
 
     def get_queryset(self):
         return self.queryset
 
     def create(self, request, *args, **kwargs):
-        post_id = request.data.get("post", None)
-        pid = request.data.get("owner", None)
-        print(post_id)
-        print(pid)
+        post_id = request.data.get("post", "")
+        pid = request.data.get("owner", "")
 
-        if not post_id or not pid:
-            return Response({"error": "Failed to provide post id and profile id"})
+        if post_id == "" or pid == "":
+            return Response(
+                {"error": "Failed to provide post id and profile id"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         post = BasePost.objects.get(id=post_id)
         owner = Profile.objects.get(id=pid)
         liked, created = Like.objects.get_or_create(post=post, owner=owner)
 
         if created:
-            return Response({"error": "Invalid action, double like"})
+            return Response(
+                {"error": "Invalid action, double like"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         post.liked.add(owner)
 
-        return Response(data={"success": "like added to"})
+        return Response(
+            data={"success": "Post was liked"}, status=status.HTTP_201_CREATED
+        )
 
-    def destroy(self, request, *args, **kwargs):
-        # TODO: figure out the way to overrid delete route by using pid and thid
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(data={"success": "deleted"})
+    @action(
+        methods=["delete"],
+        detail=False,
+        url_path="remove-like",
+        url_name="remove_like",
+    )
+    def remove_like(self, request):
+        post_id = request.query_params.get("post_id", "")
+        pid = request.query_params.get("pid", "")
+
+        if post_id == "" or pid == "":
+            return Response(
+                data={"error": "Query params must contain post_id and pid!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            post = BasePost.objects.get(id=post_id)
+            owner = Profile.objects.get(id=pid)
+            instance = self.queryset.get(post=post_id, owner=pid)
+            instance.delete()
+            post.liked.remove(owner)
+
+        except Exception as e:
+            return Response(
+                data={"error": "Query failed {}".format(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(data={"success": "User was unliked"}, status=status.HTTP_200_OK)
 
 
 class MemorizeViewSet(viewsets.ModelViewSet):
@@ -126,26 +155,61 @@ class MemorizeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        thid = self.request.query_params.get("thid", None)
-        pid = self.request.query_params.get("pid", None)
+        tid = self.request.query_params.get("tid", "")
+        pid = self.request.query_params.get("pid", "")
 
-        if not thid or not pid:
+        if tid == "" or pid == "":
             return Response(
-                data={"error": "Failed to provide thread id and profile id"}
+                data={"error": "Query params must contain tid and pid"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
-        memorize, created = self.queryset.get_or_create(thread=thid, owner=pid)
+        thread = Thread.objects.get(id=tid)
+        owner = Profile.objects.get(id=pid)
+        memorize, created = self.queryset.get_or_create(thread=thread, owner=owner)
 
         if created:
-            return Response(data={"message": "The thread was memorized!"})
+            return Response(
+                data={"message": "The thread was memorized!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        Thread.objects.get(id=thid).add(liked=pid)
+        thread.memorized.add(owner)
 
-    def perform_destroy(self, instance):
-        thid = self.request.query_params.get("thid", None)
-        pid = self.request.query_params.get("pid", None)
-        instance.delete(thread=thid, owner=pid)
-        Thread.objects.get(thread=thid).remove(liked=pid)
+        return Response(
+            data={"success": "Thread was memorized"}, status=status.HTTP_201_CREATED
+        )
+
+    @action(
+        methods=["delete"],
+        detail=False,
+        url_path="remove-memorize",
+        url_name="remove-memorize",
+    )
+    def remove_memorize(self, request):
+        tid = request.query_params.get("tid", "")
+        pid = request.query_params.get("pid", "")
+
+        if tid == "" or pid == "":
+            return Response(
+                data={"error": "Query params must contain tid and pid"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            thread = BasePost.objects.get(id=tid)
+            owner = Profile.objects.get(id=pid)
+            instance = self.queryset.get(thread=thread, owner=pid)
+            instance.delete()
+            thread.memorized.remove(owner)
+
+        except Exception as e:
+            return Response(
+                data={"error": "Query failed {}".format(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(data={"success": "User was unliked"}, status=status.HTTP_200_OK)
 
 
 class TagViewSet(viewsets.ModelViewSet):
