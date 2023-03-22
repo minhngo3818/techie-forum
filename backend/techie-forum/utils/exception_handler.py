a1b25f31d6391536a7b46f14ac16f44424f436b6
minhngo3818 <minhngo2814@gmail.com>
2023-03-21 23:42:53 -0700

    modify exception handler in backend

--- backend/techie-forum/utils/exception_handler.py
+++ backend/techie-forum/utils/exception_handler.py
@@ -10,18 +10,13 @@ def custom_exception_handler(exception, context) -> Response:
     response = exception_handler(exception, context)
 
     if response is not None:
-        http_code_message = {s.value: s.description for s in HTTPStatus}
-        status_code = response.status_code
-        error_payload = {"error": {"status_code": 0, "message": "", "details": {}}}
+        error_payload = {"details": {}}
         error_list = {}
 
         for label, message in response.data.items():
             error_list[label] = message
 
-        error = error_payload["error"]
-        error["status_code"] = status_code
-        error["message"] = http_code_message[status_code]
-        error["details"] = error_list
+        error_payload["details"] = error_list
         response.data = error_payload
 
     return response
