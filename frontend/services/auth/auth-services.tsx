4fc3750af36ef785d15a4fecd46f4fd8fffdd6c7
= <minhngo2814@gmail.com>
2023-04-01 10:57:00 -0700

    remove unused services

--- frontend/services/auth/auth-services.tsx
+++ frontend/services/auth/auth-services.tsx
@@ -8,31 +8,6 @@ import {
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// Hanle login logic and retrieve user data
-export async function loginService(data: LoginInterface) {
-  const response = await axiosInst.post("/user/login", data, {
-    withCredentials: true,
-  });
-
-  return response;
-}
-
-// Hanle logout and return to homepage
-export async function logoutService() {
-  const response = await axiosInst.post("/user/logout", {
-    withCredentials: true,
-  });
-
-  return response;
-}
-
-// Handle user registration logic
-export async function registerService(data: RegisterInterface) {
-  const response = await axiosInst.post("user/register", data);
-
-  return response;
-}
-
 /**
  * Fetch to get csrftoken for any post requests
  */
@@ -79,49 +54,4 @@ export async function requestVerifyEmail() {
  */
 export function verifyEmail(token: string) {
   return axiosInst.get(`user/email-verification?token=${token}`);
-}
-
-// Handle forgot password and send email to reset password
-export async function forgotPasswordService(data: { email: string }) {
-  const response = await axiosInst.post("/user/reset-password-request", data, {
-    withCredentials: true,
-  });
-  return response;
-}
-
-// Hanle reset password via email, unauthorized access
-export async function resetPasswordService(data: ResetPasswordInterface) {
-  const response = await axiosInst.post("/user/password-reset-complete", data, {
-    withCredentials: true,
-  });
-
-  return response;
-}
-
-// Handle change password, authorized access
-export async function changePasswordService(data: ChangePasswordInterface) {
-  const response = await axiosInst.post("/user/auth/change-password", data, {
-    withCredentials: true,
-  });
-  return response;
-}
-
-// Set account to inactive
-export async function deactivateAccountService() {
-  const response = await axiosInst.post(
-    "user/auth/",
-    { is_active: false },
-    {
-      withCredentials: true,
-    }
-  );
-
-  return response;
-}
-
-// Delete account
-export async function deleteAccountService() {
-  const response = await axiosInst.post("user/auth/delete-account/");
-
-  return response;
 }
