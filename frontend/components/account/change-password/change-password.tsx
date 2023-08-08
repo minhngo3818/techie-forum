17e0f3dbb2110de51ba3321920e5b934be7e6a1b
= <minhngo2814@gmail.com>
2023-08-07 20:52:26 -0700

    change password ui

--- frontend/components/account/change-password/change-password.tsx
+++ frontend/components/account/change-password/change-password.tsx
@@ -1,44 +1,77 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Router from "next/router";
+import { changePassword } from "../../../services/auth/auth-services";
 import BaseField from "../../form/field-base/base-field";
 import styles from "../../../styles/Account.module.css";
 import { EventTargetNameValue } from "../../../interfaces/forum/form/form-field";
+import { BarLoader } from "react-spinners";
+import { IChangePasswordForm } from "../../../interfaces/user/auth-interface";
+
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,30}$/;
+
+const pwdChangeMode = {
+  LOADING: "LOADING",
+  CHANGE: "CHANGE",
+  VIEW: "VIEW",
+  ERROR: "ERROR",
+};
 
 export default function ChangePassword() {
-  const [passwords, setPasswords] = useState({
-    oldPassword: "",
+  const [passwords, setPasswords] = useState<IChangePasswordForm>({
+    old_password: "",
     password: "",
     password2: "",
   });
+  const [guiMode, setGuiMode] = useState(pwdChangeMode.VIEW);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [isPasswordMatch, setPasswordMatch] = useState(false);
+
   const oldPasswordRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const password2Ref = useRef<HTMLInputElement>(null);
 
-  const [isChangeMode, setChangeMode] = useState(false);
-  const [isVerifying, setVerifying] = useState(false);
+  useEffect(() => {
+    setIsValidPassword(PASSWORD_REGEX.test(passwords.password));
+    setPasswordMatch(passwords.password === passwords.password2);
+  }, [passwords.password, passwords.password2]);
 
-  const handleChangePassword = useCallback(
-    ({ target: { name, value } }: EventTargetNameValue) => {
-      setPasswords((passwords) => ({ ...passwords, [name]: value }));
-    },
-    []
-  );
+  const handleChangePassword = ({
+    target: { name, value },
+  }: EventTargetNameValue) => {
+    setPasswords((passwords) => ({ ...passwords, [name]: value }));
+  };
 
-  const hanldeSubmitPassword = () => {};
+  const hanldeSubmitPassword = async () => {
+    setGuiMode(pwdChangeMode.LOADING);
+    let status = pwdChangeMode.VIEW;
+    try {
+      await changePassword(passwords);
+    } catch {
+      status = pwdChangeMode.ERROR;
+    } finally {
+      setTimeout(() => {
+        setGuiMode(status);
+      }, 1200);
+      Router.reload();
+    }
+  };
 
   return (
     <div className={styles.accountSection}>
       <h3 className={styles.accountSecTitle}>Password</h3>
-      {isChangeMode && (
+      {guiMode === pwdChangeMode.CHANGE && (
         <form>
           <BaseField
             innerRef={oldPasswordRef}
             name="oldPassword"
             label="Old Password"
             type="password"
-            value={passwords.oldPassword}
+            value={passwords.old_password}
             onChange={handleChangePassword}
             placeholder="Enter old password"
             fieldType="input"
+            required={true}
           />
           <BaseField
             innerRef={passwordRef}
@@ -49,7 +82,37 @@ export default function ChangePassword() {
             onChange={handleChangePassword}
             placeholder="Enter new password"
             fieldType="input"
+            isValid={isValidPassword}
+            isRegrex={true}
+            required={true}
           />
+          {passwords.password !== "" && !isValidPassword && (
+            <ul className="w-[90%] flex flex-col justify-start text-smoke text-sm">
+              <li>♦️ Password must contain at least 8 characters</li>
+              <li>
+                ♦️ Include upper & lowercase letters, a number & a special
+                character.
+              </li>
+              <li>
+                ♦️ Allowed special characters:
+                <span aria-label="exclamation mark" className="ml-1">
+                  !
+                </span>
+                <span aria-label="at symbol" className="ml-1">
+                  @
+                </span>
+                <span aria-label="hashtag" className="ml-1">
+                  #
+                </span>
+                <span aria-label="dollar sign" className="ml-1">
+                  $
+                </span>
+                <span aria-label="percent" className="ml-1">
+                  %
+                </span>
+              </li>
+            </ul>
+          )}
           <BaseField
             innerRef={password2Ref}
             name="password2"
@@ -59,6 +122,9 @@ export default function ChangePassword() {
             onChange={handleChangePassword}
             placeholder="Re-enter new password"
             fieldType="input"
+            isValid={isPasswordMatch}
+            isRegrex={true}
+            required={true}
           />
           <div className={styles.accountSecBtnWrapper}>
             <button
@@ -69,22 +135,53 @@ export default function ChangePassword() {
             </button>
             <button
               className={`${styles.accountSecBtn} ${styles.accountSecBtnSmall}`}
-              onClick={() => setChangeMode(!isChangeMode)}
+              onClick={() => setGuiMode(pwdChangeMode.VIEW)}
             >
               Cancel
             </button>
           </div>
         </form>
       )}
-      {!isChangeMode && (
+      {guiMode === pwdChangeMode.VIEW && (
         <>
           <p className={styles.accountText}>******</p>
           <div className={styles.accountSecBtnWrapper}>
             <button
               className={`${styles.accountSecBtn} ${styles.accountSecBtnNormal}`}
-              onClick={() => setChangeMode(!isChangeMode)}
+              onClick={() => setGuiMode(pwdChangeMode.CHANGE)}
             >
               Change
+            </button>
+          </div>
+        </>
+      )}
+      {guiMode === pwdChangeMode.LOADING && (
+        <>
+          <p className={styles.accountText}>Waiting for response...</p>
+          <BarLoader
+            color="#ffffff"
+            width="100%"
+            aria-label="Loading BarLoader"
+          />
+        </>
+      )}
+      {guiMode === pwdChangeMode.ERROR && (
+        <>
+          <p className={styles.accountText}>
+            Failed to send request ¯\_(ツ)_/¯. Please try again
+          </p>
+          <div className={styles.accountSecBtnWrapper}>
+            <button
+              className={`${styles.accountSecBtn} ${styles.accountSecBtnSmall}`}
+              onClick={hanldeSubmitPassword}
+            >
+              Try again
+            </button>
+            <button
+              className={`${styles.accountSecBtn} ${styles.accountSecBtnSmall}`}
+              onClick={() => setGuiMode(pwdChangeMode.VIEW)}
+            >
+              Cancel
             </button>
           </div>
         </>
--- frontend/services/auth/auth-services.tsx
+++ frontend/services/auth/auth-services.tsx
@@ -1,17 +1,19 @@
 import axiosInst from "../axios/axios-instance";
 import { toastResponse } from "../../utils/toast-helper";
+import axios from "axios";
+import { IChangePasswordForm } from "../../interfaces/user/auth-interface";
 
 /**
  * Request an verification email if user don't receive an email
  * @param email an email verification token
- * @returns axios get promise
+ * @returns axios GET promise
  */
 export async function requestVerifyEmail() {
   let udsf = sessionStorage.getItem("udsf"); // Use a substitution in later update
   return axiosInst
     .get(`user/email-verification/request/${udsf}`)
     .then(() => {
-      toastResponse("success", "Email verificationr request was sent");
+      toastResponse("success", "Email verification request was sent");
     })
     .catch((error) => {
       toastResponse("error", error.message);
@@ -19,12 +21,12 @@ export async function requestVerifyEmail() {
 }
 
 /**
- * Perform get request to verify user email
+ * Perform GET request to verify user email
  * Use wrap function in order to be consumed by useQuery hook
  * @param token an email verification token
- * @returns axios get promise
+ * @returns axios GET promise
  */
-export function verifyEmail(token: string) {
+export async function verifyEmail(token: string) {
   return axiosInst
     .get(`user/email-verification?token=${token}`)
     .then(() => {
@@ -41,12 +43,12 @@ export function verifyEmail(token: string) {
 }
 
 /**
- * Perform a put request for changing user email
+ * Perform a PUT request for changing user email
  * State unverified email in user object in session Storage
  * @param email
- * @returns axios put promise
+ * @returns axios PUT promise
  */
-export function changeEmail(email: string) {
+export async function changeEmail(email: string) {
   return axiosInst
     .put(`user/change-email`, { email: email })
     .then(() => {
@@ -57,6 +59,22 @@ export function changeEmail(email: string) {
         user.is_verified = false;
         sessionStorage.setItem("techie:traits", JSON.stringify(user));
       }
+    })
+    .catch((error) => {
+      toastResponse("error", error.message);
+    });
+}
+
+/**
+ * Change user password word with auth
+ * @param data: a change password form
+ * @returns axios PUT promise
+ */
+export async function changePassword(data: IChangePasswordForm) {
+  return axiosInst
+    .put("user/change-password", data)
+    .then(() => {
+      toastResponse("success", "Your password has changed");
     })
     .catch((error) => {
       toastResponse("error", error.message);
