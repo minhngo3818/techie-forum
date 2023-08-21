72d638b97b8e570dbad2ae7903c3296a976e0a73
= <minhngo2814@gmail.com>
2023-08-20 20:52:14 -0700

    delay toast message

--- frontend/utils/toast-helper.tsx
+++ frontend/utils/toast-helper.tsx
@@ -1,10 +1,19 @@
 import { ToastOptions, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export function toastResponse(type: string, message: string) {
+const DELAY_INCREMENT = 200;
+const AUTO_CLOSE_TIME = 2000;
+
+interface MessageObject {
+  [key: string]: string;
+}
+
+export function toastResponse(type: string, message: string, delay?: number) {
   let defaultConfigs = {
     position: "top-center",
     hideProgressBar: true,
+    delay: delay,
+    autoClose: AUTO_CLOSE_TIME,
   } as ToastOptions;
 
   switch (type) {
@@ -23,5 +32,17 @@ export function toastResponse(type: string, message: string) {
     default:
       toast(message, defaultConfigs);
       break;
+  }
+}
+
+export function toastDelayedSeqMessage(type: string, object: MessageObject) {
+  let delay = DELAY_INCREMENT;
+
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      toastResponse(type, `${object[key]}`, delay);
+    }
+
+    delay += DELAY_INCREMENT;
   }
 }
