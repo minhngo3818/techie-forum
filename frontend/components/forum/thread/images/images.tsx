3884b45ac7f08c71c64a51400de7bf5ba8fa20ed
= <minhngo2814@gmail.com>
2023-08-20 20:55:36 -0700

    thread image list component

--- frontend/components/forum/thread/images/images.tsx
+++ frontend/components/forum/thread/images/images.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./ThreadImage.module.css";
 
 interface ThreadImageType {
-  isEdit: boolean;
-  images: string[];
+  images?: string[] | FileList;
 }
 
 export default function ThreadImages(props: ThreadImageType) {
-  return <div className={styles.threadImagesWrapper}></div>;
+  if (Array.isArray(props.images)) {
+    return (
+      <div className={styles.threadImages}>
+        {props.images?.map((image, index) => {
+          return (
+            <div key={index}>
+              <Image src={image as string} alt="content-image" fill />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return <></>;
 }
