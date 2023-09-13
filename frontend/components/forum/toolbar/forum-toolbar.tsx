11f63b1a2fe82cb7c9e45289d5c2033f0e436483
= <minhngo2814@gmail.com>
2023-09-13 16:34:26 -0700

    forum toolbar

---
+++ frontend/components/forum/toolbar/forum-toolbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./ForumToolbar.module.css";
+
+export default function ForumToolbar({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  return <div className={styles.forumToolbar}>{children}</div>;
+}
