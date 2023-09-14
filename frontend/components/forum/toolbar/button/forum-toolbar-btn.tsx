323846bf8b49fb4ad7cc56659f0d0e7f6ce4c2e8
= <minhngo2814@gmail.com>
2023-09-13 18:06:33 -0700

    forum toolbar btn

---
+++ frontend/components/forum/toolbar/button/forum-toolbar-btn.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import IconButton from "@components/utils/buttons/icon-button/icon-button";
+import styles from "./ForumToolbarBtn.module.css";
+
+interface ForumToolbarBtn {
+  label: string;
+  name: string;
+  onClick: () => void;
+  isActive: boolean;
+}
+
+export default function ForumToolbarBtn(props: ForumToolbarBtn) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth < 640) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="my-2">
+        <IconButton
+          keyId="forum-btn"
+          name={props.name}
+          content={props.label}
+          isState={props.isActive}
+          setState={props.onClick}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <button
+      type="button"
+      className={`${styles.FTBtn} ${props.isActive && styles.FTBtnActive}`}
+      onClick={props.onClick}
+    >
+      {props.label}
+    </button>
+  );
+}
