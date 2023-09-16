a9a47cdb0a1040d990115dc309980606ea7a2918
= <minhngo2814@gmail.com>
2023-09-16 00:18:46 -0700

    install tailwind patterns

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,28 @@ module.exports = {
       sm: "375px",
       md: "640px",
     },
+    patterns: {
+      opacities: {
+        100: "1",
+        80: ".80",
+        60: ".60",
+        40: ".40",
+        20: ".20",
+        10: ".10",
+        5: ".05",
+      },
+      sizes: {
+        1: "0.25rem",
+        2: "0.5rem",
+        4: "1rem",
+        6: "1.5rem",
+        8: "2rem",
+        16: "4rem",
+        20: "5rem",
+        24: "6rem",
+        32: "8rem",
+      },
+    },
     extend: {
       colors: {
         smoke: "#CCCCCC",
@@ -35,5 +57,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-bg-patterns")],
 };
