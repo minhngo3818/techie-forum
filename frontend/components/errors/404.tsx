fc6d74605e9b0649b0c195532c326a3536b9b0c5
= <minhngo2814@gmail.com>
2023-09-22 21:26:54 -0700

    error component

---
+++ frontend/components/errors/404.tsx
@@ -0,0 +1,7 @@
+export default function Error404() {
+  return (
+    <div className="flex w-full justify-center items-center">
+      <p className="text-2xl text-smoke font-medium">404 Not Found</p>
+    </div>
+  );
+}
